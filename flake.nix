2aa82cc6c3a6a09fe23c8bea0dfd882dc32ca70f
Ryan Schlesinger <ryan@ryanschlesinger.com>
2023-06-29 13:03:09 -0300

    Add support for non-flake systems

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, flake-compat, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       # debug = true;
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
